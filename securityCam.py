71d658810486a574644310391af05faf4c009497
HussainMadarwala53 <70691828+HussainMadarwala53@users.noreply.github.com>
2021-07-15 21:37:49 +0530

    Update securityCam.py

--- securityCam.py
+++ securityCam.py
@@ -12,6 +12,7 @@ def takeSnapshot():
         ret,frame = videoCaptureObject.read()
         img_name = "img"+str(number)+".png"
         cv2.imwrite(img_name,frame)
+        start_time = time.time
         result = False
         return img_name
     print("snapshot taken")
@@ -22,7 +23,7 @@ def uploadFile(img_name):
     access_token = 'k7hFWglJ5JsAAAAAAAAAATK0YUp8s_aoSuhB564d7kVSscn-8uPUxSsq9jXtr5Mu'
     file=img_name
     fileFrom=file
-    fileto="/test1/"+img_name
+    fileto="/test1/"+(img_name)
     dbx = dropbox.Dropbox(access_token)
 
     with open(fileFrom,'rb') as f:
